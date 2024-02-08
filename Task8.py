645d7244809885c434d9190a34e66296dc4fdab2
lugovtsov <lugovtsov_ak@orbitasporta.com>
2024-02-08 18:42:09 +0700

    Task8

---
+++ Task8.py
@@ -0,0 +1,5 @@
+totalaccount = int(input("Введите общую сумму счета за обед: "))
+totalnumpeople = int(input("Введите общее количество участников обеда: "))
+
+answer = totalaccount / totalnumpeople
+print("Каждый участник обеда должен заплатить: ", answer)
