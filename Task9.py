35e59ef856a61e8e1f832f693048b3e382ab2ac8
lugovtsov <lugovtsov_ak@orbitasporta.com>
2024-02-08 19:07:52 +0700

    Task9

---
+++ Task9.py
@@ -0,0 +1,8 @@
+days = int(input("Введите количество дней: "))
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print("В", days,"днях будет", hours,"часов")
+print("В", days,"днях будет", minutes,"минут")
+print("В", days,"днях будет", seconds,"секунд")
