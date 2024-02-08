a80c357d24e1313545cf5cbfba28423a90807524
lugovtsov <lugovtsov_ak@orbitasporta.com>
2024-02-08 17:06:31 +0700

    Task5

---
+++ Task5.py
@@ -0,0 +1,8 @@
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+num3 = int(input("Введите третье число: "))
+
+sumnum = num1 + num2
+multisum = sumnum * num3
+
+print("Результат вычисления равен ", multisum)
