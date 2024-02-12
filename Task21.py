7dada1c5b2e93befa02d5d566bb963ea5fd4f196
lugovtsov <lugovtsov_ak@orbitasporta.com>
2024-02-12 23:49:32 +0700

    Task21, Task22

---
+++ Task21.py
@@ -0,0 +1,4 @@
+name = str(input("Введите Ваше Имя: "))
+first_name = str(input("Введите Вашу Фамилию: "))
+
+print(name, first_name, f"в Вшем Имени и Фамилии {len(name) + len(first_name)} букв")
---
+++ Task22.py
@@ -0,0 +1,3 @@
+name = str(input("Введите Ваше имя в нижнем регистре: "))
+firstname = str(input("Введите Вашу фамилию: "))
+print(f'{name.capitalize()} {firstname.capitalize()}')
