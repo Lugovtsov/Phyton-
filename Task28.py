0019b4bbd9b4f2905270c185e69d17937aaa6677
lugovtsov <lugovtsov_ak@orbitasporta.com>
2024-03-09 21:32:53 +0700

    Task27, Task28, Task29, Task30

---
+++ Task28.py
@@ -0,0 +1,3 @@
+number = float(input("Введите дробное число: "))
+new_number = number * 2
+print(round(new_number, 2))
---
+++ Task29.py
@@ -0,0 +1,5 @@
+import math
+
+number = int(input("Введите число больше 500: "))
+answer = math.sqrt(number)
+print(round(answer, 2))
